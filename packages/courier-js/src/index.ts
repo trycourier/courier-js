621ce97378044b825a3f8c0f29201dc5e6884b9f
Suhas Deshpande <suhas@courier.com>
2023-03-10 09:05:12 -0800

    Adds v1.0.0
    supports following methods
    
      - identify
      - subscribe
      - unsubscribe

---
+++ packages/courier-js/src/index.ts
@@ -0,0 +1,35 @@
+import { Courier, CourierOptions } from "./helpers/client";
+
+const client = {
+  __instance: null as Courier | null,
+  init(options: CourierOptions) {
+    this.__instance = new Courier(options);
+  },
+  get instance() {
+    if (!this.__instance) {
+      throw new Error("Courier instance not initialized");
+    }
+    return this.__instance;
+  },
+  async identify(userId: string, payload: Record<string, unknown>) {
+    if (!userId) {
+      throw new Error("userId is required");
+    }
+    await this.instance.post(`identify/${userId}`, payload);
+  },
+  // apply common decorator to check if __instance is initialized
+  async subscribe(userId: string, listId: string) {
+    if (!userId || !listId) {
+      throw new Error("userId is required");
+    }
+    await this.instance.put(`lists/${listId}/subscribe/${userId}`);
+  },
+  async unsubscribe(userId: string, listId: string) {
+    if (!userId || !listId) {
+      throw new Error("userId is required");
+    }
+    this.instance.delete(`lists/${listId}/unsubscribe/${userId}`);
+  },
+};
+
+export default client;
